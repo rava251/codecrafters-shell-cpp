e5887de4d365c1543923ce5e42a4ce8f0fbf59c5
Alina <anitalozova@gmail.com>
2025-11-15 13:38:06 +0300

    Step 1: Print input string and exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,10 @@
 #include <iostream>
-#include "shell.h"
-#include <signal.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <pwd.h>
-
-Shell* g_shell = nullptr;
-
-void sighup_handler(int sig) {
-    if (g_shell) {
-        std::cout << "Configuration reloaded" << std::endl;
-    }
-}
+#include <string>
 
 int main() {
-    Shell shell;
-    g_shell = &shell;
-    
-    signal(SIGHUP, sighup_handler);
-    
-    const char* home_dir = getenv("HOME");
-    if (!home_dir) {
-        home_dir = getpwuid(getuid())->pw_dir;
+    std::string input;
+    if (std::getline(std::cin, input)) {
+        std::cout << input << std::endl;
     }
-    
-    std::string users_dir = std::string(home_dir) + "/users";
-    mkdir(users_dir.c_str(), 0755);
-    std::cout << "Created: " << users_dir << std::endl;
-    
-    return shell.run();
+    return 0;
 }
